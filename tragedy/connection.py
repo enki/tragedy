ffaaa47d52a7b19ec78b4ccc9523d518510da745
Paul Böhm <paul@boehm.org>
2010-04-07 04:49:59 -0700

    cleanup

--- tragedy/connection.py
+++ tragedy/connection.py
@@ -6,10 +6,3 @@ def connect(*args, **kwargs):
     global client
     client = pycassa.connect(*args, **kwargs)
     return client
-    
-    # gatherinfo()
-
-# def gatherinfo(client=None):
-#     if not client:
-#         client = globals()['client']
-#     print client.describe_keyspace('BBQ')
