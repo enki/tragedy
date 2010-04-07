b11e2e60c52723780c8f6d074c55a011bd5a6979
Paul Böhm <paul@boehm.org>
2010-04-07 04:51:07 -0700

    cleanup

--- tragedy/util.py
+++ tragedy/util.py
@@ -1,6 +1,5 @@
 import sys
 import traceback
-import pycassa
 import threading
 import time
 
@@ -71,32 +70,3 @@ def unhandled_exception_handler(reraise=False):
     if reraise:
         raise
     print '>>>>>>>>>>>>>>>>'
-
-class ObjWithFakeDictAndKey(object):
-    __slots__ = ['realdict', 'key']
-    
-    def __init__(self, realdict, key):
-        self.realdict = realdict
-        self.key = key
-    
-    def __getattr__(self, name):
-        if name == '__dict__':
-            return self.realdict
-        elif name == 'name':
-            return self.name
-        else:
-            raise AttributeError('wrong getattr %s' % name)
-
-try:
-    import collections
-    BestDictAvailable = collections.OrderedDict
-except:
-    unhandled_exception_handler()
-    BestDictAvailable = dict
-
-# def create_instance(cls, **kwargs):
-#     instance = cls()
-#     instance.update(kwargs)
-#     return instance
-# 
-# pycassa.columnfamilymap.create_instance = create_instance    
